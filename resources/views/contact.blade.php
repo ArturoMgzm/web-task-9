1ad34fe5e194fa7b85354620a8fc8bf6410c5a4b
Arturo Manrique <arturomgzm@hotmail.com>
2024-04-19 16:48:15 -0600

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Contacts')
+
+@section('content_header')
+    <h1>Contacts</h1>
+@stop
+
+@section('content') 
+    <p>This is my contacts view.</p> 
+@stop
---
+++ resources/views/photos.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Photos')
+
+@section('content_header')
+    <h1>Photos</h1>
+@stop
+
+  @section('content') 
+    <p>This is my photos view.</p> 
+@endsection 
